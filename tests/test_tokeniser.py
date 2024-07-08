e7da5128aeb7c645a48b67b40cb82df03fedbec5
Basel <baselalnassr8@gmail.com>
2024-07-08 13:43:37 +0300

    change tokeniser tests to use raw strings

--- tests/test_tokeniser.py
+++ tests/test_tokeniser.py
@@ -3,47 +3,47 @@ from jsonparser.parser import tokeniser
 
 class TestTokeniser(unittest.TestCase):
     def test_one_key(self):
-        tokens = tokeniser("{\"key\": \"value\"}")
+        tokens = tokeniser(r'{"key": "value"}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", "value", "}"])
 
     def test_multiple_keys(self):
-        tokens = tokeniser("{\"key\": \"value\", \"key2\": \"value\"}")
+        tokens = tokeniser(r'{"key": "value", "key2": "value"}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", "value", ",", "key2", ":", "value", "}"])
 
     def test_int_value(self):
-        tokens = tokeniser("{\"key\": 250}")
+        tokens = tokeniser(r'{"key": 250}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", 250, "}"])
 
     def test_negative_number_value(self):
-        tokens = tokeniser("{\"key\": -250}")
+        tokens = tokeniser(r'{"key": -250}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", -250, "}"])
 
     def test_float_value(self):
-        tokens = tokeniser("{\"key\": 10.4}")
+        tokens = tokeniser(r'{"key": 10.4}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", 10.4, "}"])
 
     def test_bool_value(self):
-        tokens = tokeniser("{\"key\": true}")
+        tokens = tokeniser(r'{"key": true}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", True, "}"])
 
     def test_null_value(self):
-        tokens = tokeniser("{\"key\": null}")
+        tokens = tokeniser(r'{"key": null}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key", ":", None, "}"])
 
     def test_multiple_datatypes(self):
-        tokens = tokeniser("{\"key1\": true, \"key2\": false, \"key3\": null, \"key4\": \"value\", \"key5\": 101}")
+        tokens = tokeniser(r'{"key1": true, "key2": false, "key3": null, "key4": "value", "key5": 101}')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key1", ":", True, ",", "key2", ":", False, ",", "key3", ":", None, ",",  "key4", ":", "value", ",", "key5", ":", 101, "}"])
     
     def test_empty_array(self):
-        tokens = tokeniser("{\"key1\": [] }")
+        tokens = tokeniser(r'{"key1": [] }')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key1", ":", "[", "]", "}"])
 
     def test_array_with_items(self):
-        tokens = tokeniser("{\"key1\": [1, 2, 3] }")
+        tokens = tokeniser(r'{"key1": [1, 2, 3] }')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key1", ":", "[", 1, ",", 2, ",", 3, "]", "}"])
 
     def test_strings_with_newline(self):
-        tokens = tokeniser('{\"key1\": "Hello\n" }')
+        tokens = tokeniser(r'{"key1": "Hello\n" }')
         self.assertEqual(list(map(lambda t: t.value, tokens)), ["{", "key1", ":", "Hello\n", "}"])
 
     def test_inner_double_quote(self):
